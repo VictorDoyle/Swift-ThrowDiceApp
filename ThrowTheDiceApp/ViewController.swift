2dde2cf5a0ff60c90c3b5e883391961c2dbd7283
Victor Doyle <victor-doyle@outlook.com>
2021-03-25 13:44:48 -0400

    refactored code

--- ThrowTheDiceApp/ViewController.swift
+++ ThrowTheDiceApp/ViewController.swift
@@ -19,9 +19,14 @@ class ViewController: UIViewController {
 
     //MARK: Roll the dice button section
     @IBAction func rollDiceButton(_ sender: UIButton) {
-        diceImageView1.image =  [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")] [Int.random(in: 1...5)]
         
-        diceImageView2.image = [ #imageLiteral(resourceName: "DiceSix"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceOne")] [Int.random(in: 1...5)]
+        //base array
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        // randomElement or Int.random(in: 0...5)
+        diceImageView1.image =  diceArray.randomElement()
+        
+        diceImageView2.image = diceArray.randomElement()
         
         
     }
