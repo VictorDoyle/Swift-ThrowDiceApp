acb89103ba2c83fa1b83cc90065784e4fb13d03d
Victor Doyle <victor-doyle@outlook.com>
2021-03-25 02:39:10 -0400

    appDelegate update with WillTerminate func

--- ThrowTheDiceApp/AppDelegate.swift
+++ ThrowTheDiceApp/AppDelegate.swift
@@ -10,11 +10,13 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
     // MARK: UISceneSession Lifecycle
